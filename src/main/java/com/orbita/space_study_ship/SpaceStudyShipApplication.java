ed8ef114ed7db1c724beba175aec5569c06d08e2
yeo-li <parkseongyeol2110@gmail.com>
2025-12-29 12:47:09 +0900

    Initial commit

---
+++ src/main/java/com/orbita/space_study_ship/SpaceStudyShipApplication.java
@@ -0,0 +1,13 @@
+package com.orbita.space_study_ship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceStudyShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceStudyShipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orbita/space_study_ship/SpaceStudyShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbita.space_study_ship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceStudyShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
