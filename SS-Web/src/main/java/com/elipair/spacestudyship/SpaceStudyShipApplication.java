baa025b6ec367240c0b6cef7b67b5decefc5abf3
EM-H20 <ghd0106@gmail.com>
2026-02-12 17:25:18 +0900

    init

---
+++ SS-Web/src/main/java/com/elipair/spacestudyship/SpaceStudyShipApplication.java
@@ -0,0 +1,13 @@
+package com.elipair.spacestudyship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceStudyShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceStudyShipApplication.class, args);
+	}
+
+}
---
+++ SS-Web/src/test/java/com/elipair/spacestudyship/SpaceStudyShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elipair.spacestudyship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceStudyShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
