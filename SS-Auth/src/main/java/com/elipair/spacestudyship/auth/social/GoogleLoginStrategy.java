6615e9299e89a6abbfe618711967783fd9fb2766
EM-H20 <ghd0106@gmail.com>
2026-05-19 14:18:03 +0900

    소셜 로그인 Firebase IdToken 검증 적용 : refactor : 코드 리뷰 반영 - GoogleLoginStrategy Firebase 토큰 검증 실패 로그 추가

--- SS-Auth/src/main/java/com/elipair/spacestudyship/auth/social/GoogleLoginStrategy.java
+++ SS-Auth/src/main/java/com/elipair/spacestudyship/auth/social/GoogleLoginStrategy.java
@@ -6,8 +6,10 @@ import com.elipair.spacestudyship.member.constant.SocialType;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class GoogleLoginStrategy implements SocialLoginStrategy {
@@ -19,6 +21,7 @@ public class GoogleLoginStrategy implements SocialLoginStrategy {
         try {
             return firebaseAuth.verifyIdToken(socialIdToken).getUid();
         } catch (FirebaseAuthException e) {
+            log.warn("[GoogleLogin] Firebase 토큰 검증 실패 | reason={}", e.getMessage());
             throw new CustomException(ErrorCode.INVALID_TOKEN);
         }
     }
